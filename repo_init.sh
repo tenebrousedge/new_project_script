ba5a71dc5a2d73bef12f616ace73839e4c1b4c3e
Patrick "Kai" Leahy <tenebrousedge@gmail.com>
2017-03-22 09:48:54 -0700

    Initial commit for new project script
    This script should create a basic page structure for a bootstrap site using bower.

---
+++ repo_init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "enter a project name:"
+read projectname
+
+mkdir -p $HOME/Desktop/$projectname/{img,js,css}
+touch $HOME/Desktop/$projectname/{css/styles.css,js/scripts.js,index.html}
+cd $HOME/Desktop/$projectname
+git init
+bower init
+bower install bootstrap
+>>index.html <<'EOM'
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="bower_components/bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <script src="bower_components/bootstrap/dist/css/bootstrap.css"></script>
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="js/scripts.js"></script>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+
+EOM
+atom .
+exit 0
