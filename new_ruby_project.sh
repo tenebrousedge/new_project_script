82684f470c87b50adef31537d688393523f50587
Kai Leahy <tenebrousedge@gmail.com>
2017-04-19 12:25:40 -0700

    add lots of things to the new_ruby_project script
    add layout.erb, app.rb, index.rb
    add atom opening
    fix bug with layout.erb
    add sinatra and sinatra-contrib to Gemfile
    make files get created in the right place
    set -u so that bad variable names cause an error

--- new_ruby_project.sh
+++ new_ruby_project.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+set -u
 
 read -p "Enter a project name:" projectname
 
 read -p "Enter where you want the project files to be created: [default=$HOME/Desktop]" basedir
 basedir=${basedir:-"$HOME/Desktop"}
-echo "creating folders"
-mkdir -p $basedir/$projectname/{lib,spec}
-echo "creating files"
 
+echo "creating folders"
+mkdir -p $basedir/$projectname/{lib,spec,views}
+
+echo "creating files"
+cd $basedir/$projectname
 cat >Gemfile  <<'EOM'
 source "https://rubygems.org"
 
 gem 'rspec'
+gem 'sinatra'
+gem 'sinatra-contrib'
 
 group :development do
   gem 'pry'
+  gem 'capybara'
 end
 EOM
 
@@ -26,9 +32,39 @@ RSpec::Core::RakeTask.new do |task|
 end
 EOM
 
-cd $basedir/$projectdir
-bundle install >/dev/null
+cat >./view/layout.erb <<'EOM'
+<!DOCTYPE html>
+  <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <%= yield %>
+  </body>
+</html>
+EOM
+
+cat >app.rb <<'EOM'
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get('/') do
+  erb(:index)
+end
+EOM
+
+touch ./view/index.erb
+
+bundle install >/dev/null &
 git init
-if [ ! -f "$HOME/.pairs"]
+if [ ! -f "$HOME/.pairs"]; then
   nano "$HOME/.pairs"
 fi
+
+atom .
